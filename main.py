012a013374fb8e43a273d387ee7c58ec0d54a302
jurbanowicz <jurbanowicz@student.agh.edu.pl>
2025-05-27 21:14:48 +0200

    main update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from beamngpy import BeamNGpy, Scenario, Vehicle, angle_to_quat
+from training import Training
 
 # Instantiate BeamNGpy instance running the simulator from the given path,
 # communicating over localhost:25252
@@ -9,12 +10,12 @@ bng.open()
 print("Connected!")
 print("Loading scenario...")
 
-scenario = Scenario("italy", "rl_training")
+scenario = Scenario("smallgrid", "rl_training")
 vehicle = Vehicle("main_car", model="sunburst2", license="PYTHON")
 
 # Add it to our scenario at this position and rotation
 scenario.add_vehicle(vehicle, 
-        pos=(463.45, 1210.24, 167.23),
+        pos=(0, 0, 0),
         rot_quat=angle_to_quat((0, 0, 90))
         ) 
 # Place files defining our scenario for the simulator to read
@@ -24,6 +25,9 @@ scenario.make(bng)
 bng.scenario.load(scenario)
 bng.scenario.start()
 # Make the vehicle's AI span the map
+trainer = Training(bng, scenario, vehicle)
+trainer.start()
+
 input('Hit Enter when done...')
 
 # Disconnect BeamNG
